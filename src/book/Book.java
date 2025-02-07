3893fb73cdadb5bea05461fd27118cc58df64bc4
tri <2015iann@gmail.com>
2025-02-06 19:25:40 -0600

    Added getter and setter methods for Book class, added parameterized constructor

--- src/book/Book.java
+++ src/book/Book.java
@@ -1,5 +1,47 @@
 package book;
 
 public class Book {
-
+	public Book(String title, String author, String ISBN, double price) {
+		this.title = title;
+		this.author = author;
+		this.ISBN = ISBN;
+		this.price = price;
+	}
+	
+	
+	private String title = "Unknown";
+	private String author = "Unknown";
+	private String ISBN = "Unknown";
+	private double price = 0.0;
+	
+	
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String ISBN) {
+		this.ISBN = ISBN;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
 }
